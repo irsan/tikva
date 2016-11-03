6ca65df34586b75d7ee75b112ff1ce2c4cdea19e
Irsan Jie <irsan@mavenlab.com>
2016-11-03 23:06:52 +0800

    add properties.json

--- app.js
+++ app.js
@@ -1,46 +1,71 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const Bunyan = require('bunyan')
+const BodyParser = require('body-parser');
+const CookieParser = require('cookie-parser');
+const Express = require('express');
+const Favicon = require('serve-favicon');
+const Logger = require('morgan');
+const Mongoose = require('mongoose');
+const Path = require('path');
+const Session = require('express-session');
+const Redis = require("redis");
+
+const log = Bunyan.createLogger({ name : 'tikva:index' });
+const mode = process.env.MODE ? process.env.MODE : "local";
+PROPERTIES = JSON.parse(FS.readFileSync('./resources/properties.json', 'utf8'))[mode];
+log.info(mode, PROPERTIES);
+
+var RedisStore = require('connect-redis')(Session);
 
 var index = require('./routes/index');
 var users = require('./routes/admin');
 
-var app = express();
+var app = Express();
+
+var redisStore = new RedisStore({
+    client: redis
+});
+
+var session = Session({
+    secret: YAHYA_PROPERTIES.session_secret,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    store: redisStore,
+    resave: true,
+    saveUninitialized: true
+});
+
+app.use(session);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', Path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(Logger('dev'));
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: false}));
+app.use(CookieParser());
+app.use(Express.static(Path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
