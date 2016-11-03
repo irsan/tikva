d079f403f598f327771b2b869296ae919716264e
Irsan Jie <irsan@mavenlab.com>
2016-11-04 00:19:30 +0800

    with callback

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const Bunyan = require('bunyan');
+const Express = require('express');
+const Vasync = require('vasync');
+const YahyaCommon = require('yahya-common');
+const YahyaModel = require('yahya-model');
+const Request = require('request');
+
+const log = Bunyan.createLogger({ name : 'tikva:routes/index' });
+
+var router = Express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+/* GET home page. */
+router.post('/fb_callback', (req, res, next) => {
+    if(req.query.key != PROPERTIES['yahya-fb-callback'].key) {
+        res.status(404);
+        res.send("Invalid key");
+    }
+    next();
+}, (req, res) => {
+    log.info("body", req.body);
+    res.send("Ok");
 });
 
 module.exports = router;
