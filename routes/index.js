6db0ddbf67b34637a4773d371a0fff0c1118f698
Irsan Jie <irsan@mavenlab.com>
2016-11-04 15:18:47 +0800

    getting info

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const Bunyan = require('bunyan');
 const Express = require('express');
+const Request = require('request');
 const Vasync = require('vasync');
 
 const log = Bunyan.createLogger({ name : 'tikva:routes/index' });
@@ -13,7 +14,23 @@ router.get('/', function (req, res, next) {
 
 /* GET home page. */
 router.post('/fb_callback', (req, res, next) => {
-    req.installed = false;
+    if(!req.installed) {
+        // var key = req.query.key;
+        //
+        // var options = {
+        //     url : PROPERTIES.yahyaFB.url + "/" + key + "/user/" +
+        // };
+
+        log.info("THE INFO", JSON.stringify(req.body));
+
+        return res.send("Ok");
+    }
+    next();
+}, (req, res, next) => {
+    if(req.query.key != PROPERTIES['yahya-fb-callback'].key) {
+        res.status(404);
+        res.send("Invalid key");
+    }
     next();
 }, (req, res) => {
     log.info("body", req.body);
