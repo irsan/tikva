4f7bc77e26837d97be71e9c0e9b4140dd16cf00d
Irsan Jie <irsan@mavenlab.com>
2016-11-11 17:42:45 +0800

    install.js

--- install.js
+++ install.js
@@ -36,22 +36,24 @@ Vasync.waterfall([
                 },
                 (callback) => {//prompt user to input mongo redis and witKey
                     Prompt.start();
-                    Prompt.get(['mongo', 'redis', 'witKey'], callback);
+                    Prompt.get(['mongo', 'redis', 'witKey', 'gavriFBKey'], callback);
                 },
-                (promptResult, callback) => {//
-                    console.log("PROMPT RESULT:", promptResult);
+                (answer, callback) => {//
+                    console.log("PROMPT RESULT:", answer);
 
                     var properties = {
                         adminKey : key,
-                        mongodb : promptResult.mongo,
+                        mongodb : answer.mongo,
                         redis : {
-                            url : promptResult.redis
+                            url : answer.redis
                         },
                         wit : {
-                            key : promptResult.witKey
+                            key : answer.witKey
                         },
-                        fb : {
-                            graphAPI : "https://graph.facebook.com/v2.6"
+                        gavri : {
+                            fb : {
+                                key : answer.gavriFBKey
+                            }
                         },
                         "session_secret" : RandomString.generate(16)
                     };
@@ -62,54 +64,6 @@ Vasync.waterfall([
         } else {
             callback();
         }
-    },
-    (callback) => {
-        var properties = JSON.parse(FS.readFileSync('./resources/properties.json', 'utf8'));
-        Mongoose.connect(properties.mongodb);
-        Mongoose.Promise = global.Promise;
-
-        var schema = {
-            properties : {
-                gavriFBKey : {
-                    description : 'Key for Gavri FB'
-                }
-            }
-        };
-
-        Prompt.start();
-        Prompt.get(schema, callback);
-    },
-    (answer, callback) => {//create admin webhook
-        Model.Webhook({
-            name : 'Admin',
-            verifyToken : RandomString.generate(16)
-        }).save((error, webhook) => {
-            if(error) {
-                return callback(error);
-            }
-
-            var shasum = Crypto.createHash('sha256');
-            shasum.update(webhook._id.toString() + webhook.name + webhook.verifyToken + new Date().getTime());
-            webhook.key = shasum.digest('hex');
-            webhook.save();
-            callback(null, {
-                webhook : webhook,
-                pageAccessToken : answer.pageAccessToken
-            });
-        });
-    },
-    (data, callback) => {//create admin page
-        Model.Page({
-            name            : "Gavri FB Admin Page",
-            pageAccessToken : data.pageAccessToken,
-            adminPage       : true,
-            webhook         : data.webhook
-        }).save((error) => {
-            if(error) {
-                return callback(error);
-            }
-            callback();
-        });
     }
 ], (error) => {
     if(error) {
