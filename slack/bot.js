b20603b5570ca582aaaf596605b638d4ad04665e
Irsan Jie <irsan@mavenlab.com>
2017-04-28 18:47:06 +0800

    test attachment

--- slack/bot.js
+++ slack/bot.js
@@ -94,6 +94,42 @@ class Bot {
 
         Slack.chat.postMessage({
             token, channel, username, text, as_user: true,
+            "attachments": [
+                {
+                    "text": "Choose a game to play",
+                    "fallback": "You are unable to choose a game",
+                    "callback_id": "wopr_game",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "game",
+                            "text": "Chess",
+                            "type": "button",
+                            "value": "chess"
+                        },
+                        {
+                            "name": "game",
+                            "text": "Falken's Maze",
+                            "type": "button",
+                            "value": "maze"
+                        },
+                        {
+                            "name": "game",
+                            "text": "Thermonuclear War",
+                            "style": "danger",
+                            "type": "button",
+                            "value": "war",
+                            "confirm": {
+                                "title": "Are you sure?",
+                                "text": "Wouldn't you prefer a good game of chess?",
+                                "ok_text": "Yes",
+                                "dismiss_text": "No"
+                            }
+                        }
+                    ]
+                }
+            ]
         }, (err, data) => {
             console.log("POST MESSAGE TO CHANNEL", err, data, text);
         });
