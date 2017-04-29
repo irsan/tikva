5dcb947e3809de08532b0e1c2a04b3fa17d35d83
Irsan Jie <irsan@mavenlab.com>
2017-04-29 20:59:02 +0800

    constructor for command controller

--- controller/command_controller.js
+++ controller/command_controller.js
@@ -3,7 +3,9 @@ const Vasync = require('vasync');
 
 class CommandController {
 
-    log = Bunyan.createLogger({ name : 'tikva:controller/CommandController '});
+    constructor() {
+        this.log = Bunyan.createLogger({ name : 'tikva:controller/CommandController '});
+    }
 
     parseCommand(command, callback) {
         callback(null, "Ok, your command is " + command);
