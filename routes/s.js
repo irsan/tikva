0ff033357fad5c96f798fc67192f847d6a83334b
Irsan Jie <irsan@mavenlab.com>
2017-05-01 00:04:20 +0800

    s_add_ftv

--- routes/s.js
+++ routes/s.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/add_ftv', (req, res) => {
-    res.render('s_index', {title: 'Tikva'});
+    res.render('s_add_ftv', {title: 'Tikva'});
 });
 
 module.exports = router;
