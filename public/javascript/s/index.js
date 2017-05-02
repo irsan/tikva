31407e8d2baa8b9a6c919a454869d6b8822d2237
Irsan Jie <irsan@mavenlab.com>
2017-05-02 18:10:19 +0800

    toggle left

---
+++ public/javascript/s/index.js
@@ -0,0 +1,39 @@
+var app = angular.module('TikvaApp', ['ngMaterial']);
+
+app.controller('mainController', function($scope, $mdSidenav) {
+
+    $scope.toggleLeft = buildDelayedToggler('left');
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+        var timer;
+
+        return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+                timer = undefined;
+                func.apply(context, args);
+            }, wait || 10);
+        };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+        return debounce(function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                });
+        }, 200);
+    }
+});
