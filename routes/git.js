3f4f105f68f2d7e84b5717dfffde5886335078d5
Irsan Jie <irsan@mavenlab.com>
2017-05-02 11:13:24 +0800

    signature

--- routes/git.js
+++ routes/git.js
@@ -1,4 +1,5 @@
 const Bunyan = require('bunyan');
+const crypto = require('crypto');
 const Express = require('express');
 const SimpleGit = require('simple-git');
 const Vasync = require('vasync');
@@ -9,6 +10,10 @@ const router = Express.Router();
 
 router.post('/pull', (req, res) => {
     log.info("GIT PULL WITH BODY: ", req.headers, req.body);
+
+    let signature = crypto.createHmac('sha1', 'yucca-orchid-avert-tight-began').update(req.body.payload).digest('hex');
+
+    log.info("THE SIGNATURE IS: ", signature);
 
     if(req.headers['x-hub-signature'] != "sha1=db1d44b1b8660ccb0665d99c0bc1d0bba3b73d0f") {
         return res.status(404).send("Not Found");
