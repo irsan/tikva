cff1ef44a97bc99ed78eefd8b84f7b98676c3b07
Irsan Jie <irsan@mavenlab.com>
2017-05-02 10:50:35 +0800

    git pull

---
+++ routes/git.js
@@ -0,0 +1,26 @@
+const Bunyan = require('bunyan');
+const Express = require('express');
+const SimpleGit = require('simple-git');
+const Vasync = require('vasync');
+
+const log = Bunyan.createLogger({ name : 'tikva:routes/git' });
+
+const router = Express.Router();
+
+router.get('/pull/:token', (req, res) => {
+    if(req.params.token != PROPERTIES.vault.slackVerificationToken) {
+        return res.status(404).send("Not Found");
+    }
+
+    let simpleGit = SimpleGit('./');
+    simpleGit.pull((error, result) => {
+        log.info("GIT PULL", error, result);
+        if(error) {
+            return res.status(500).send(error);
+        }
+
+        res.send(result);
+    });
+});
+
+module.exports = router;
