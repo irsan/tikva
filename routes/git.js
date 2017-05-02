06792f65acb7401e4e94ae831d80a9941a1a05ea
Irsan Jie <irsan@mavenlab.com>
2017-05-02 11:50:02 +0800

    pull secret in vault

--- routes/git.js
+++ routes/git.js
@@ -11,12 +11,8 @@ const router = Express.Router();
 router.post('/pull', (req, res) => {
     log.info("GIT PULL WITH BODY: ", req.headers);
 
-    let hmac = Crypto.createHmac("sha1", "yucca-orchid-avert-tight-began");
-    // console.log("CREATE HMAC");
+    let hmac = Crypto.createHmac("sha1", PROPERTIES.vault.gitWebhookSecret);
     let calculatedSignature = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
-    // console.log(req.headers["x-hub-signature"] === calculatedSignature); // Returns false
-    // console.log(req.headers["x-hub-signature"]) // => sha1=blablabla
-    // console.log(calculatedSignature);
 
     if(req.headers["x-hub-signature"] !== calculatedSignature) {
         return res.status(404).send("Not Found");
