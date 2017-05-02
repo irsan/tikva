ddcf6614050f6538d7826b3317d0990e3854d80b
Irsan Jie <irsan@mavenlab.com>
2017-05-02 12:45:42 +0800

    serviceDate

---
+++ public/javascript/s/config.js
@@ -0,0 +1,11 @@
+var app = angular.module('TikvaApp');
+
+app.config(['$mdDateLocaleProvider', function ($mdDateLocaleProvider) {
+    $mdDateLocaleProvider.formatDate = function (date) {
+        return date ? moment(date).format('DD/MM/YYYY') : '';
+    };
+    $mdDateLocaleProvider.parseDate = function (dateString) {
+        var m = moment(dateString, 'DD/MM/YYYY', true);
+        return m.isValid() ? m.toDate() : new Date(NaN);
+    };
+}]);
