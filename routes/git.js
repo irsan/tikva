d04c8e9c323df4bfef8422aa35420d0741e767c9
Irsan Jie <irsan@mavenlab.com>
2017-05-02 11:40:54 +0800

    setting the secret

--- routes/git.js
+++ routes/git.js
@@ -11,7 +11,7 @@ const router = Express.Router();
 router.post('/pull', (req, res) => {
     log.info("GIT PULL WITH BODY: ", req.headers);
 
-    let hmac = crypto.createHmac("sha1", process.env.WEBHOOK_SECRET);
+    let hmac = crypto.createHmac("sha1", "yucca-orchid-avert-tight-began");
     let calculatedSignature = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
     console.log(req.headers["x-hub-signature"] === calculatedSignature); // Returns false
     console.log(req.headers["x-hub-signature"]) // => sha1=blablabla
