4ad159cbca150379242dbef62c7182a86ad08db5
Irsan Jie <irsan@mavenlab.com>
2017-05-03 14:29:21 +0800

    followups

--- public/javascript/s/ctrl_followups.js
+++ public/javascript/s/ctrl_followups.js
@@ -1,5 +1,7 @@
 var app = angular.module('TikvaApp');
 
-app.controller('followupsCtrl', function ($scope, $log) {
+app.controller('followupsCtrl', function ($scope, $rootScope, $log) {
+    $rootScope.selectedMenu = 'followups';
+
     $log.debug("FOLLOW UPS CTRL");
 });
