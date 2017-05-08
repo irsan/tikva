8ffb6e9d34a995a0a14863709cff6c09c936df9b
Irsan Jie <irsan@mavenlab.com>
2017-05-08 21:08:19 +0800

    sunday

--- public/javascript/s/ctrl_followups.js
+++ public/javascript/s/ctrl_followups.js
@@ -7,7 +7,7 @@ app.controller('followupsCtrl', function ($scope, $rootScope, $location, $log, r
     $log.debug("FOLLOW UPS CTRL");
 
     $scope.data = {
-        sunday : new Date(),
+        sunday : moment().startOf('week').toDate(),
         followUps : {
             followUps : []
         }
