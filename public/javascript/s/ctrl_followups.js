8075df857d1eefeb6a3116ffca71779a06ea2b0c
Irsan Jie <irsan@mavenlab.com>
2017-05-13 08:45:08 +0800

    followUp

--- public/javascript/s/ctrl_followups.js
+++ public/javascript/s/ctrl_followups.js
@@ -121,6 +121,7 @@ app.controller('newFollowUpCtrl', function($scope, $rootScope, $mdDialog, $locat
                 if(resp.status == 200) {
                     var data = resp.data;
                     if(data.status == "Ok") {
+                        $scope.data.followUp.profileImage = data.file.location;
                         // var uploadId = data.data._id;
                         // rest.country.import(uploadId, function(response) {
                         //     if(response.status == "Ok") {
