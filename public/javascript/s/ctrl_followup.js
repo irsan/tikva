f8edee61909a304098e8ec8cb1e089d2a6e67c2c
Irsan Jie <irsan@mavenlab.com>
2017-05-31 13:52:54 +0800

    query sp

--- public/javascript/s/ctrl_followup.js
+++ public/javascript/s/ctrl_followup.js
@@ -89,18 +89,19 @@ app.controller('followUpAssignDialogCtrl', function($scope, $mdDialog, $log, res
             });
         },
         listSPs : function() {
-            $log.info("THE CARECELLLLLSSSS", $scope.data.carecell, $scope.data.filter.allCarecells || $scope.data.filter.carecells.length > 0);
-            let queries = $scope.data.filter.carecells.length > 0 ? {
-                carecells : $scope.data.filter.carecells
-            } : {};
-            rest.sp.list(queries, function (response) {
-                $log.info("SP LIST RESPONSE", response);
-                if (response.status == "Ok") {
-                    $scope.data.sps = response.data.sps;
-                    $scope.show.sps = $scope.data.filter.allCarecells || $scope.data.filter.carecells.length > 0;
-                    $log.info($scope.show);
-                }
-            });
+            if($scope.data.carecell) {
+                let queries = {
+                    carecells : [ $scope.data.carecell._id ]
+                };
+                rest.sp.list(queries, function (response) {
+                    $log.info("SP LIST RESPONSE", response);
+                    // if (response.status == "Ok") {
+                    //     $scope.data.sps = response.data.sps;
+                    //     $scope.show.sps = $scope.data.filter.allCarecells || $scope.data.filter.carecells.length > 0;
+                    //     $log.info($scope.show);
+                    // }
+                });
+            }
         },
         init : function() {
             this.listCarecells();
