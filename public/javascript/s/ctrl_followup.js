f62a3d390b53236b06d7339c222770e6ef571b81
Irsan Jie <irsan@mavenlab.com>
2017-05-31 13:59:20 +0800

    list sp

--- public/javascript/s/ctrl_followup.js
+++ public/javascript/s/ctrl_followup.js
@@ -96,11 +96,9 @@ app.controller('followUpAssignDialogCtrl', function($scope, $mdDialog, $log, res
                 $log.info("CACEEEEEEEEEEE", $scope.data.carecell, queries);
                 rest.sp.list(queries, function (response) {
                     $log.info("SP LIST RESPONSE", response);
-                    // if (response.status == "Ok") {
-                    //     $scope.data.sps = response.data.sps;
-                    //     $scope.show.sps = $scope.data.filter.allCarecells || $scope.data.filter.carecells.length > 0;
-                    //     $log.info($scope.show);
-                    // }
+                    if (response.status == "Ok") {
+                        $scope.data.sps = response.data.sps;
+                    }
                 });
             }
         },
